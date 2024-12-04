def22a7b64d52838153cd5c18c323ccc125fa3d1
miguelbcdev <miguelbcdev@gmail.com>
2024-12-04 21:48:47 +0000

    segundo comit

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
-<?php
-for ($i = 1; $i <= 10; $i++) {
-    echo "<tr>";
-    for ($j = 1; $j <= 10; $j++) {
-        echo "<td>" . ($i * $j) . "</td></br>";
-    }
-    echo "</tr>";
-}
-?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Hello World</title>
+  <link href='/style.css' rel='stylesheet'>
+</head>
+<body>
+  <h1>Titulo</h1>
+  <button onclick="funcion()">Press me</button>
+  <?php echo '</br>'; include_once 'controller.php';?>
+  <script defer type="text/javascript" src="/main.js"></script>
+</body>
+</html>
